a8b5865dfb69bcaa50822910f472bf46a4c57c10
周世婧 <s20171105133@ciecimac17deimac.local>
2018-06-21 09:18:27 +0800

    Initial Commit

---
+++ c++ 6.21/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++ 6.21
+//
+//  Created by 周世婧 on 18/6/21.
+//  Copyright © 2018年 周世婧. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
